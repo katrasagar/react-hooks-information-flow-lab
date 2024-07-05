764bd4fef208051a949bbdec6d342a8aacf88a69
Your Name <aqatra6@gmail.com>
2024-07-05 09:13:22 +0300

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header({isDarkMode,onDarkModeClick}){
+    return(
+        <Header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {isDarkMode? "Dark":"Light"}Mode
+
+
+            </button>
+        </Header>
+    )
+}
+export default Header;
